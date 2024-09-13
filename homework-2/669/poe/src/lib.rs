722d9e94915fb87a6b5cff1786b4243ee4c52f30
Kurtis Moxley <image1224@outlook.com>
2024-09-14 01:18:08 +0800

    669: Finish homework-2.

---
+++ homework-2/669/poe/src/lib.rs
@@ -0,0 +1,106 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{ensure, pallet_prelude::*, sp_runtime::BoundedVec};
+	use frame_system::pallet_prelude::{BlockNumberFor, *};
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		
+		#[pallet::constant]
+		type MaxClaimLength: Get<u32>;
+	}
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	#[pallet::storage]
+	pub type Proofs<T: Config> = StorageMap<
+		_, 
+		Blake2_128Concat, 
+		BoundedVec<u8, T::MaxClaimLength>,
+		(T::AccountId, BlockNumberFor<T>),
+	>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		ClaimCreated(T::AccountId, BoundedVec<u8, T::MaxClaimLength>),
+		ClaimRevoked(T::AccountId, BoundedVec<u8, T::MaxClaimLength>),
+	}
+
+	#[pallet::error]
+	pub enum Error<T>{
+		ProofAlreadyExist,
+		ClaimNotExist,
+		NotClaimOwner,
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::call_index(0)]
+		#[pallet::weight({0})]
+		pub fn create_claim(origin: OriginFor<T>, claim: BoundedVec<u8, T::MaxClaimLength>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(
+				!Proofs::<T>::contains_key(&claim),
+				Error::<T>::ProofAlreadyExist,
+			);
+
+			Proofs::<T>::insert(
+				&claim, 
+				(sender.clone(), frame_system::Pallet::<T>::block_number()));
+
+			Self::deposit_event(Event::<T>::ClaimCreated(sender, claim));
+
+			Ok(().into())
+		}
+
+		#[pallet::call_index(1)]
+		#[pallet::weight({0})]
+		pub fn revoke_claim(origin: OriginFor<T>, claim: BoundedVec<u8, T::MaxClaimLength>) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			let (owner, _) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			Proofs::<T>::remove(&claim);
+
+			Self::deposit_event(Event::ClaimRevoked(sender, claim));
+
+			Ok(().into())
+		}
+	
+		#[pallet::call_index(2)]
+		#[pallet::weight({0})]
+		pub fn transfer_claim(
+			origin: OriginFor<T>,
+			claim: BoundedVec<u8, T::MaxClaimLength>,
+			dest: T::AccountId,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			let (owner, _block_number) =
+				Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			Proofs::<T>::insert(&claim, (dest, frame_system::Pallet::<T>::block_number()));
+
+			Ok(().into())
+		}
+	}
+}
