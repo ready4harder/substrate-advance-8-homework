231369279a625c94062b4dbcdbdbfe74171852fa
vlbos <vlbos2018@gmail.com>
2024-09-18 12:15:26 +0800

    Add task3

---
+++ homework-3/655/pallets/kitties/src/hooks.rs
@@ -0,0 +1,82 @@
+use frame_support::pallet_macros::pallet_section;
+
+/// Define all hooks used in the pallet.
+#[pallet_section]
+mod hooks {
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn on_runtime_upgrade() -> Weight {
+            Weight::default()
+        }
+
+        fn on_initialize(n: BlockNumberFor<T>) -> Weight {
+            log::info!("Kitties on_initialize at block {:?}", n);
+            // KittyOnSale::<T>::iter().for_each | (kitty_id, until_block) | {
+            //     if until_block == n {
+            //         let ower = KittyOwner::<T>::get(kitty_id).expect("");
+            //         if let Some(bids) = KittiesBid::<T>::take(kitty_id) {
+            //             let mut new_owner = None;
+            //             let mut final_price = BalanceOf::<T>::min_value();
+            //             for bid in &bids {
+            //                 T::Currency::unreserve(&bid.0, bid.1);
+            //                 if bid.1 > final_price {
+            //                     final_price = bid.1;
+            //                     new_owner = Some(bid.0);
+            //                 }
+            //             }
+            //         }
+            //         if final_price != BalanceUf::<T>::min_value() {
+            //             T::Currency::transfer(
+            //                 new_owner.as_ref().umarap(),
+            //                 &owner,
+            //                 final_price,
+            //                 ExistenceRequirenent::KeepATive,
+            //             )
+            //             .expect("");
+            //             KittyOwner::<T>::insert(kitty_id, new_owner.unwrap());
+            //         }
+            //     }
+            // };
+            let _ = Self::trade(n);
+            Weight::default()
+        }
+
+        fn on_poll(n: BlockNumberFor<T>, _remaining_weight: &mut WeightMeter) {
+            log::info!("Kitties on_poll at block {:?}", n);
+        }
+
+        fn on_finalize(n: BlockNumberFor<T>) {
+            // remove the kitty on sale if no bid and the block number is greater than the until_block.
+            // sale the kitty if according to bid price
+            log::info!("Kitties on_finalize at block {:?}", n);
+        }
+
+        fn on_idle(n: BlockNumberFor<T>, _remaining_weight: Weight) -> Weight {
+            log::info!("Kitties on_idle at block {:?}", n);
+            Weight::default()
+        }
+
+        fn integrity_test() {
+            assert!(NextKittyId::<T>::get() == 0);
+        }
+
+        fn offchain_worker(n: BlockNumberFor<T>) {
+            log::info!("Kitties offchain_worker at block {:?}", n);
+        }
+
+        #[cfg(feature = "try-runtime")]
+        fn pre_upgrade() -> Result<Vec<u8>, TryRuntimeError> {
+            unimplemented!()
+        }
+
+        #[cfg(feature = "try-runtime")]
+        fn post_upgrade(_state: Vec<u8>) -> Result<(), TryRuntimeError> {
+            unimplemented!()
+        }
+
+        #[cfg(feature = "try-runtime")]
+        fn try_state(_n: BlockNumberFor<T>) -> Result<(), TryRuntimeError> {
+            unimplemented!()
+        }
+    }
+}
